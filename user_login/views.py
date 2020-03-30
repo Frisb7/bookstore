fbec6138b1f9682350274989e46c52b653632223
FrisbTDW <mrmunion123@gmail.com>
2020-03-30 19:56:43 +0400

    login and registration completed

--- user_login/views.py
+++ user_login/views.py
@@ -2,19 +2,23 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from account.form import AccountCreationForm
+from account.models import Account
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 def loginuser(request) :
     if request.method == 'POST' :
-        username = request.POST.get('username')
+        email = request.POST.get('email')
         password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, username=email, password=password)
         if user is not None :
             login(request, user)
             return(redirect('store'))
         else :
-            messages.info(request, 'Username OR Password is incorrect')
+            context = {'email':email}
+            messages.info(request, 'Email OR Password is incorrect')
+            return(render(request, 'user_login/login_cont.html', context))
     return(render(request, 'user_login/login.html'))
 
 def logoutuser(request) :
@@ -25,9 +29,31 @@ def registeruser(request) :
     form = AccountCreationForm()
     if request.method == 'POST' :
         form = AccountCreationForm(request.POST)
+        current = request.POST
         if form.is_valid() :
             form.save()
-            return(redirect('login'))
             messages.info(request, 'User was created successfully')
+            email = current['email']
+            context = {'email':email}
+            return(render(request, 'user_login/login_cont.html', context))
+        else :
+            account = Account.objects.all().values('email', 'username', 'first_name', 'last_name', 'grno')
+            for i in account :
+                if i['email'] == current['email'] :
+                    messages.info(request, 'A account already exists with this Email')
+                    break
+                if i['username'] == current['username'] :
+                    messages.info(request, 'A account already exists with this Username')
+                    break
+                if i['grno'] == current['grno'] :
+                    messages.info(request, 'A account already exists with this Grno')
+                    break
+            if current['password1'] == current['password2'] :
+                if len(current['password1']) < 8 :
+                    messages.info(request, 'Password must be 8 characters long')
+            else :
+                messages.info(request, 'Confirm password doesnt match the original')
+            context = {'form':form}
+            return(render(request, 'user_login/register_cont.html', context))
     context = {'form':form}
     return(render(request, 'user_login/register.html', context))
